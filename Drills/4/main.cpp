8e5befc2d6426149531aec8f891ea870a617087c
Veronika Stephanie <Veronika Stephanie>
2016-11-03 20:11:48 +0700

    add

--- Drills/4/main.cpp
+++ Drills/4/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -13,11 +14,11 @@ public:
     void setX (int x);
     int getY();
     void setY (int y);
-    int getXY();
+    int *getXY();
     void setXY (int x, int y);
     string toString();
     double distance(int x, int y);
-    double distance(MyPoint another);
+    double distance(MyPoint &another);
     double distance();
 };
 
@@ -53,11 +54,13 @@ void MyPoint::setY (int y)
     this->y = y;
 }
 
-int MyPoint::getXY()
+int *MyPoint::getXY()
 {
     int *arr;
-    arr = new MyPoint[2];
-    return *arr;
+    arr = new int[2];
+    arr[0] = this->x;
+    arr[1] = this->y;
+    return arr;
 }
 
 void MyPoint::setXY (int x, int y)
@@ -65,21 +68,35 @@ void MyPoint::setXY (int x, int y)
     this->x = x;
     this->y = y;
 }
-string toString()
+string MyPoint::toString()
 {
-    return "(" + x + "," + y + ")";
+    return "(" + to_string(x) + "," + to_string(y) + ")";
 }
 
-double distance(int x, int y)
+double MyPoint::distance(int x, int y)
+{
+    return sqrt(pow((this->x)-x,2)+pow((this->y)-y,2));
+}
+
+double MyPoint::distance(MyPoint &another)
+{
+    return MyPoint::distance(another.x, another.y);
+}
+
+double MyPoint::distance()
 {
 
 }
-    double distance(MyPoint another);
-    double distance();
 
 
 int main()
 {
+    MyPoint one (1,2);
+    cout << one.getX() << endl;
+    cout << one.getY() << endl;
+    cout << one.distance (3,4);
+    MyPoint two (3,7);
+    cout << one.distance (two);
 
     return 0;
 }
